546b444fc03917620a6cec8ca036f1c4b4725d16
Patrick Doyle <patrickdoyle30@gmail.com>
2019-08-05 22:26:18 -0500

    Initial Commit

---
+++ ViewifyFourSquareProjectOneTests/ViewifyFourSquareProjectOneTests.swift
@@ -0,0 +1,34 @@
+//
+//  ViewifyFourSquareProjectOneTests.swift
+//  ViewifyFourSquareProjectOneTests
+//
+//  Created by Patrick Doyle on 8/5/19.
+//  Copyright © 2019 Patrick Doyle. All rights reserved.
+//
+
+import XCTest
+@testable import ViewifyFourSquareProjectOne
+
+class ViewifyFourSquareProjectOneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
